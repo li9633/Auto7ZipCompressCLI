2e41e3991d1e809d52e9a4aa8d6f5d3bcb820a08
li9633 <1367429633@qq.com>
2024-03-20 00:46:48 +0800

    完成路径与文件格式化

--- main.py
+++ main.py
@@ -7,24 +7,28 @@ def formatCompressPath(compressPath: list[str]):
     for i in range(0, len(compressPath)):
         if os.path.isdir(compressPath[i]):
             compressPath[i] += "\\"
-        elif os.path.isfile(compressPath[i]):
-            compressPath[i] = compressPath[i].split(".")[0]
     return compressPath
 
 
-def generateCLI(password, compresslevel, compressPath, size: str = '15g'):
-    generStr = f"7z a -v{size} -p{password} -mx{compresslevel} -mhe=on \"{compressPath}.7z\" \"{compressPath}\"\n "
+def formatCompressFile(compressFile: list[str]):
+    for i in range(0, len(compressFile)):
+        if os.path.isfile(compressFile[i]):
+            compressFile[i] = compressFile[i].split(".")[0]
+    return compressFile
+
+
+def generateCLI(password, compresslevel, compressPath: str, compressFile: str, size: str = '15g'):
+    generStr = f"7z a -v{size} -p{password} -mx{compresslevel} -mhe=on \"{compressPath}.7z\" \"{compressFile}\"\n"
     return generStr
 
 
 def getCompressPath():
     with open("path.txt", encoding='utf-8') as f:
         compressPath: list[str] = f.read().splitlines()
-        f.close()
-        compressfile = compressPath[:]
+        compressFile = compressPath[:]
         formatCompressPath(clearAllMark(compressPath))
-        clearAllMark(compressfile)
-    return compressfile, compressPath
+        compressFile = formatCompressFile(clearAllMark(compressFile))
+    return compressFile, compressPath
 
 
 def replaceFile(runCLI):
@@ -44,21 +48,26 @@ def processCLIArgs(CLIArgs: list):
     processedArgs = []
     r_index = -1
     n_index = -1
+    lock = -1
     for i in CLIArgs:
         processedArgs.append(i)
     try:
         processedArgs.index("-r")
     except ValueError:
-        None
+        lock += 1
     else:
         r_index = processedArgs.index("-r")
 
     try:
         processedArgs.index("-n")
     except ValueError:
-        None
+        lock += 1
     else:
         n_index = processedArgs.index("-n")
+
+    if lock == True:
+        processedArgs.append("-r")
+        return processedArgs, "-r"
 
     if r_index > -1 and n_index > -1:
         method = processedArgs.pop(r_index if r_index > n_index else n_index)
@@ -69,7 +78,7 @@ def processCLIArgs(CLIArgs: list):
         return processedArgs, method
 
 
-def swtichRunMode(runCLI, method="-r"):
+def swtichRunMode(runCLI, method):
     if method == "-r":
         replaceFile(runCLI)
     else:
@@ -88,15 +97,12 @@ if __name__ == '__main__':
         print("(path.txt)目录文件不存在！\n请按任意键继续...")
         msvcrt.getch()
         os._exit(1)
-    compressfile, compressPath = getCompressPath()
-    print(compressfile)
-    print(compressPath)
-    os._exit(0)
+    compressFile, compressPath = getCompressPath()
     runCLI: list = []
     processedArgs, method = processCLIArgs(args[1:])
     processedArgs.pop(processedArgs.index(method))
     for i in range(0, len(compressPath)):
-        runCLI.append(generateCLI(1234, 0, compressPath[i]),)
+        runCLI.append(generateCLI(1234, 0,  compressFile[i], compressPath[i]))
     runCLI.insert(0, "chcp 65001\n")
     runCLI.append("pause\n")
-    # swtichRunMode(runCLI, method=method)
+    swtichRunMode(runCLI, method)
